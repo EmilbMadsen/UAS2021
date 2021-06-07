2308ce44559710e70facf052e93f571d7d8561fd
EmilbMadsen <79310292+EmilbMadsen@users.noreply.github.com>
2021-06-07 20:17:43 +0200

    Added plots

--- Exercises/Exercise2.m
+++ Exercises/Exercise2.m
@@ -12,14 +12,10 @@ Iyy = 3e-06;
 Izz = 1e-05;
 g = 9.81;
 
-
-omega1 = 0;
-omega2 = 10000;
-omega3 = 0;
-omega4 = 10000;
-
-
-
+omega1 = 10000;
+omega2 = 0;
+omega3 = 10000;
+omega4 = 0;
 
 %Starting simulation
 t_stop=10;
@@ -27,9 +23,38 @@ t_stop=10;
 out = sim('Quadcopter_sim1');
 
 
-figure()
+figure(1)
 plot3(out.x, out.y, out.z)
 xlabel('x')
 ylabel('y')
 zlabel('z')
+title('Position of UAV')
+grid
+
+
+figure(2)
+subplot(1, 3, 1)
+plot(out.t, out.phi)
+xlabel('time [s]')
+ylabel('Roll phi [rad]')
+grid
+
+subplot(1, 3, 2)
+plot(out.t, out.theta)
+xlabel('time [s]')
+ylabel('Pitch theta [rad]')
+grid
+
+subplot(1, 3, 3)
+plot(out.t, out.psi)
+xlabel('time [s]')
+ylabel('Yaw psi [rad]')
+grid
+
+sgtitle('Omega = [0, 0, 0, 0]^T') 
+
+figure(3)
+plot(out.t, out.z)
+xlabel('time [s]')
+ylabel('Z-direction [m]')
 grid
